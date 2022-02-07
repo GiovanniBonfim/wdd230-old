232aaef36517ac1aba6a1081e16bc2b59ef4a7fd
GiovanniBonfim <bon21010@byui.edu>
2022-02-07 13:14:57 -0300

    added content to the sections

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -13,3 +13,14 @@ datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
 const year = now.getFullYear();
 document.getElementById('copyright').innerHTML = `&copy ${year} | Giovanni Bonfim | WDD 230 Project <br> Last modified : ${document.lastModified}`;
+
+let banner
+let weekDay = now.getDay();
+if (weekDay === 1 || weekDay === 2){
+  document.querySelector("#banner").classList.remove("hidden");
+  console.log("We are on time") 
+}
+else {
+  document.querySelector("#banner").classList.add("hidden");
+  console.log("it worked") 
+}
