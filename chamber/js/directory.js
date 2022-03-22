0230ae7cf22f2342ce5acf67a3b7c1a744c49273
GiovanniBonfim <bon21010@byui.edu>
2022-03-22 13:09:30 -0300

    directory page

---
+++ chamber/js/directory.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("sList").onclick = () => {
+      document.getElementById("lgDemo").classList.remove("grid");
+    };
+    document.getElementById("sGrid").onclick = () => {
+      document.getElementById("lgDemo").classList.add("grid");
+    };
+  });
+  
